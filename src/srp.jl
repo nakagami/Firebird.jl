6e987993793355995f875cf5c030ca06dfaf4258
Hajime Nakagami <nakagami@gmail.com>
2021-01-23 09:40:01 +0900

    add bigint_to_bytes() and bytes_to_bigint()

--- src/srp.jl
+++ src/srp.jl
@@ -21,9 +21,36 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 ################################################################################
+using SHA
 
 const SRP_KEY_SIZE = 128
 const SRP_SALT_SIZE = 32
+
+function pad(n::BigInt)::Vector{UInt8}
+    v::Vector{UInt8} = []
+    for x in 1:SRP_KEY_SIZE
+        push!(v, UInt8(n & 255))
+        n >>= 8
+        if n == 0
+            break
+        end
+    end
+
+    reverse!(v)
+    v
+end
+
+function bigint_to_bytes(v::BigInt)::Vector{UInt8}
+    pad(v)
+end
+
+function bytes_to_bigint(v::Vector{UInt8})::BigInt
+    r::BigInt = 0
+    for ui in v
+        r = r * 256 + ui
+    end
+    r
+end
 
 function get_primce()::Tuple{BigInt, BigInt, BigInt}
     N = big"0xE67D2E994B2F900C3F41F08F5BB2627ED0D49EE1FE767A52EFCD565CD6E768812C3E1E9CE8F0A8BEA6CB13CD29DDEBF7A96D4A93B55D488DF099A15C89DCB0640738EB2CBDD9A8F7BAB561AB1B0DC1C6CDABF303264A08D1BCA932D1F1EE428B619D970F342ABA9A65793B8B2F041AE5364350C16F735F56ECBCA87BD57B29E7"
