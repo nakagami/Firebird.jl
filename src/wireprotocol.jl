8c627c0dfb9b2ba55aabe047e7d672a85ef6ea5e
Hajime Nakagami <nakagami@gmail.com>
2021-02-24 17:03:51 +0900

    remove println()

--- src/wireprotocol.jl
+++ src/wireprotocol.jl
@@ -52,7 +52,6 @@ mutable struct WireChannel
     arc4in::Union{Arc4, Nothing}
     arc4out::Union{Arc4, Nothing}
     function WireChannel(host::String, port::Int)
-        println("host=$(host), port=$(port)")
         sock = Sockets.connect(host, port)
         new(sock, nothing, nothing)
     end
