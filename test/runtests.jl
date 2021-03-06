1cba5acc6f4c4bae4b28a96954320a579e2697af
Hajime Nakagami <nakagami@gmail.com>
2021-03-06 10:45:01 +0900

    add srp test

--- test/runtests.jl
+++ test/runtests.jl
@@ -23,6 +23,9 @@
 ################################################################################
 using Test, Firebird
 
+const DEBUG_PRIVATE_KEY = big"0x60975527035CF2AD1989806F0407210BC81EDC04E2762A56AFD529DDDA2D4393"
+const DEBUG_SALT = hex2bytes("02E268803000000079A478A700000002D1A6979000000026E1601C000000054F")
+
 #@testset "connection" begin
 #    user = if haskey(ENV, "ISC_USER")
 #        ENV["ISC_USER"]
@@ -56,6 +59,19 @@ using Test, Firebird
 
     _, client_key = Firebird.get_client_proof(user, password, salt, keyA, keyB, keya, "Srp256")
     @test server_key == client_key
+
+    keyA, keya = Firebird.get_client_seed(DEBUG_PRIVATE_KEY)
+    salt = DEBUG_SALT
+    v = Firebird.get_verifier(user, password, salt)
+    keyB, keyb = Firebird.get_server_seed(v, DEBUG_PRIVATE_KEY)
+    server_key = Firebird.get_server_session(user, password, salt, keyA, keyB, keyb)
+    keyM, client_key = Firebird.get_client_proof(user, password, salt, keyA, keyB, keya, "Srp")
+    @test server_key == client_key
+    @test keyM == hex2bytes("8c12324bb6e9e683a3ee62e13905b95d69f028a9")
+
+    keyM, client_key = Firebird.get_client_proof(user, password, salt, keyA, keyB, keya, "Srp256")
+    @test server_key == client_key
+    @test keyM == hex2bytes("4675c18056c04b00cc2b991662324c22c6f08bb90beb3677416b03469a770308")
 end
 
 @testset "arc4" begin
