e0224aca9de1078a384969ccee4cfe7fc3295acc
Hajime Nakagami <nakagami@gmail.com>
2021-06-21 14:17:23 +0900

    fix connect_immediate()

--- src/execute.jl
+++ src/execute.jl
@@ -21,26 +21,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 ################################################################################
-mutable struct TextCursor{buffered} <: DBInterface.Cursor
-    conn::Connection
-    sql::String
-    nfields::Int
-    nrows::Int
-    rows_affected::Int64
-    types::Vector{Type}
-    lookup::Dict{Symbol, Int}
-    current_rownumber::Int
-    current_resultsetnumber::Int
-end
-
-
-struct TextRow{buffered} <: Tables.AbstractRow
-    cursor::TextCursor{buffered}
-    lengths::Vector{Culong}
-    rownumber::Int
-    resultsetnumber::Int
-end
-
 
 function DBInterface.execute(conn::Connection, stmt::Statement, params=[])::Cursor
     _op_execute(conn.wp, stmt.handle, conn.transaction.handle, params)
@@ -51,5 +31,6 @@ end
 
 function DBInterface.execute(conn::Connection, sql::AbstractString, params=[])
     _op_exec_immediate(conn.wp, conn.transaction.handle, sql)
+    _op_response(conn.wp)
     _op_commit(conn.wp, conn.transaction.handle)
 end
